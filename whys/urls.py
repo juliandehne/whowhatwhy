25c369330f40fe98732b24dbd017f70d495ffe8b
dehne <dehne>
2021-03-16 11:38:17 +0100

    - ported example code from CoreySchafer to current django version - changed some settings to debug mode

--- whys/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from whys import views
-
-urlpatterns = [
-    path('', views.home, name="'whys-home"),
-    path('about/', views.about, name="'blog-about"),
-]
