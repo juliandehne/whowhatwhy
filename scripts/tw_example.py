12c2f5cf32e85024ae0a6252b9b9731ac13d12ac
dehne <dehne>
2021-03-25 12:43:27 +0100

    * successfully experimented with v2 api * refactored twitter access point

--- scripts/tw_example.py
+++ scripts/tw_example.py
@@ -1,66 +1,14 @@
-from requests_oauthlib import OAuth1Session
-import os
 import json
-import yaml
-import io
-
-
-filename="C:\\Users\\julia\\PycharmProjects\\djangoProject\\twitter\\secret\\keys_simple.yaml"
-
-consumer_key = os.environ.get("CONSUMER_KEY")
-consumer_secret = os.environ.get("CONSUMER_SECRET")
-
-access_token = ""
-access_token_secret = ""
-
-with open(filename) as f:
-    my_dict = yaml.safe_load(f)
-    if consumer_key != "":
-        consumer_key = my_dict.get("consumer_key")
-    if consumer_secret != "":
-        consumer_secret = my_dict.get("consumer_secret")
-    access_token = my_dict.get("access_token")
-    access_token_secret = my_dict.get("access_token_secret")
+from twitter.util import TwitterConnector
 
 # You can adjust ids to include a single Tweets
 # Or you can add to up to 100 comma-separated IDs
-params = {"ids": "1278747501642657792", "tweet.fields": "created_at"}
+# params = {"ids": "1278747501642657792", "tweet.fields": "created_at"}
+# search_url = "https://api.twitter.com/2/tweets"
 
-# Tweet fields are adjustable.
-# Options include:
-# attachments, author_id, context_annotations,
-# conversation_id, created_at, entities, geo, id,
-# in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
-# possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
-# source, text, and withheld
+params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev', 'tweet.fields': 'author_id', 'max_results': '12'}
+search_url = "https://api.twitter.com/2/tweets/search/all"
 
-#request_token_url = "https://api.twitter.com/oauth/request_token"
-#oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-
-try:
-
-    # Make the request
-    oauth = OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=access_token,
-        resource_owner_secret=access_token_secret,
-    )
-
-    response = oauth.get(
-        "https://api.twitter.com/2/tweets", params=params
-    )
-
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
-        )
-
-    print("Response code: {}".format(response.status_code))
-    json_response = response.json()
-    print(json.dumps(json_response, indent=4, sort_keys=True))
-
-except ValueError:
-    print(
-        "There may have been an issue with the consumer_key or consumer_secret you entered."
-    )
+connector = TwitterConnector()
+json_result = connector.get_from_twitter(search_url, params, True)
+print(json.dumps(json_result, indent=4, sort_keys=True))
---
+++ twitter/util.py
@@ -0,0 +1,83 @@
+from requests_oauthlib import OAuth1Session, OAuth2Session
+import os
+import json
+import yaml
+import io
+import requests
+
+
+class TwitterConnector:
+
+    def get_from_twitter(self, search_url, params, is_oauth2=False):
+        filename = "C:\\Users\\julia\\PycharmProjects\\djangoProject\\twitter\\secret\\keys_simple.yaml"
+
+        consumer_key = os.environ.get("CONSUMER_KEY")
+        consumer_secret = os.environ.get("CONSUMER_SECRET")
+
+        # access_token = ""
+        # access_token_secret = ""
+        # bearer_token = ""
+
+        with open(filename) as f:
+            my_dict = yaml.safe_load(f)
+            if consumer_key != "":
+                consumer_key = my_dict.get("consumer_key")
+            if consumer_secret != "":
+                consumer_secret = my_dict.get("consumer_secret")
+            access_token = my_dict.get("access_token")
+            access_token_secret = my_dict.get("access_token_secret")
+            bearer_token = my_dict.get("bearer_token")
+
+        # Tweet fields are adjustable.
+        # Options include:
+        # attachments, author_id, context_annotations,
+        # conversation_id, created_at, entities, geo, id,
+        # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+        # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+        # source, text, and withheld
+
+        # request_token_url = "https://api.twitter.com/oauth/request_token"
+        # oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+        if is_oauth2:
+            headers = self.__create_headers(bearer_token)
+            json_response = self.__connect_to_endpoint(search_url, headers, params)
+            return json_response
+        else:
+            # Make the request
+            oauth = OAuth1Session(
+                consumer_key,
+                client_secret=consumer_secret,
+                resource_owner_key=access_token,
+                resource_owner_secret=access_token_secret,
+            )
+            try:
+
+                response = oauth.get(search_url, params=params)
+
+                if response.status_code != 200:
+                    raise Exception(
+                        "Request returned an error: {} {}".format(response.status_code, response.text)
+                    )
+
+                print("Response code: {}".format(response.status_code))
+                json_response = response.json()
+
+            except ValueError:
+                print(
+                    "There may have been an issue with the consumer_key or consumer_secret you entered."
+                )
+            return json_response
+
+    @staticmethod
+    def __create_headers(bearer_token):
+        headers = {"Authorization": "Bearer {}".format(bearer_token)}
+        return headers
+
+    @staticmethod
+    def __connect_to_endpoint(search_url, headers, params):
+        response = requests.request("GET", search_url, headers=headers, params=params)
+        print(response.status_code)
+        if response.status_code != 200:
+            raise Exception(response.status_code, response.text)
+        return response.json()
