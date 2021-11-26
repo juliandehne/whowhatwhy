51cccdd4189d4a0510988fc160b9d080e6e73e72
Julian Dehne <julian.dehne@gmail.com>
2021-11-26 09:35:11 +0100

    - build in fail-save for sending messages authorization error

--- delab/bot/sender.py
+++ delab/bot/sender.py
@@ -1,5 +1,7 @@
 import logging
 
+import tweepy.errors
+
 from delab.models import Tweet, PLATFORM
 from delab.tw_connection_util import send_tweet
 
@@ -7,11 +9,14 @@ logger = logging.getLogger(__name__)
 
 
 def publish_moderation(instance):
-    delab_admission = " (This was automatically send by the deliberation app)"
-    text = instance.text + delab_admission
-    parent_tweet = Tweet.objects.filter(twitter_id=instance.tn_parent_id).get()
-    if parent_tweet.platform == PLATFORM.TWITTER:
-        reply_to_id = parent_tweet.twitter_id
-        response = send_tweet(text, tweet_id=reply_to_id)
-        logger.info(response)
-        logger.info("send tweet {}".format(instance))
+    try:
+        delab_admission = " (This was automatically send by the deliberation app)"
+        text = instance.text + delab_admission
+        parent_tweet = Tweet.objects.filter(twitter_id=instance.tn_parent_id).get()
+        if parent_tweet.platform == PLATFORM.TWITTER:
+            reply_to_id = parent_tweet.twitter_id
+            response = send_tweet(text, tweet_id=reply_to_id)
+            logger.info(response)
+            logger.info("send tweet {}".format(instance))
+    except tweepy.errors.Unauthorized:
+        logger.error("could not send post out because of authorization issue")
