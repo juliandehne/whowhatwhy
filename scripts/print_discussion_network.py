097f9f3eb74604697a65baca1773c705f92c7d26
Julian Dehne <julian.dehne@gmail.com>
2022-06-24 20:47:22 +0200

    - Hamburg idea [part 3]

--- scripts/print_discussion_network.py
+++ scripts/print_discussion_network.py
@@ -1,10 +1,20 @@
 import networkx as nx
-from matplotlib import pyplot as plt
 
-from delab.network import Neo4jDAO as dao
+from delab.models import Tweet
+from delab.network.DjangoTripleDAO import DjangoTripleDAO
 
 
 def run():
-    G = dao.get_discussion_graph(1468834105718677504)
-    nx.draw(G)
-    plt.savefig("somegraph.png")
+    conversation_ids = set(Tweet.objects.values_list('conversation_id', flat=True))
+    dao = DjangoTripleDAO()
+    graphs = [(conversation_id, dao.get_discussion_graph(conversation_id)) for conversation_id in conversation_ids]
+    # non_empty_graphs = [x for x in graphs if x]
+
+    data_dir = "notebooks/followernetworks"
+    out_fp = f'{data_dir}/graphs'
+    for conversation_id, g in graphs:
+        nx.write_gpickle(g, f"{out_fp}/{conversation_id}_Gs.gpickle")
+
+    for conversation_id, g in graphs:
+        nx.read_gpickle(f"{out_fp}/{conversation_id}_Gs.gpickle")
+
