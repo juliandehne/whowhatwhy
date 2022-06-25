dc4369807961c5677552c5503e5b8e4c4ac48fd8
Julian Dehne <julian.dehne@gmail.com>
2022-06-25 12:41:57 +0200

    - Hamburg idea [part 4] - Implmented check if network already has  been downloaded

--- delab/network/conversation_network.py
+++ delab/network/conversation_network.py
@@ -1,26 +1,31 @@
+import logging
+from time import sleep
+
 import django
 
 from delab.models import Tweet, TweetAuthor
-from delab.tw_connection_util import DelabTwarc
 from delab.network.DjangoTripleDAO import DjangoTripleDAO
+from delab.tw_connection_util import DelabTwarc
+from util.abusing_lists import batch
+
+logger = logging.getLogger(__name__)
 
 
-def download_discussion_follower(conversation_id):
+def get_participants(conversation_id):
     dao = DjangoTripleDAO()
     discussion_tweets = Tweet.objects.filter(conversation_id=conversation_id).all()
     nodes = set()
     for discussion_tweet in discussion_tweets:
         # time.sleep(15)
-        user = discussion_tweet.tw_author.twitter_id
+        user = discussion_tweet.author_id
         nodes.add(user)
-    dao.add_discussion(nodes, conversation_id)
-    user_ids = [discussion_tweet.tw_author.twitter_id for discussion_tweet in discussion_tweets]
-    return user_ids
+    dao.add_discussion(nodes, conversation_id)  # this would be need in case of neo4j
+    return nodes
 
 
 def download_followers_recursively(user_ids, n_level=1, following=False):
     twarc = DelabTwarc()
-    download_followers(user_ids, twarc, n_level)
+    download_followers(user_ids, twarc, n_level, following)
 
 
 def persist_user(follower_data):
@@ -31,32 +36,45 @@ def persist_user(follower_data):
             screen_name=follower_data["username"],
             location=follower_data.get("location", "unknown")
         )
-    except django.db.utils.IntegrityError:
-        pass
+    except django.db.utils.IntegrityError as ex:
+        logger.error(ex)
 
 
 def download_followers(user_ids, twarc, n_level=1, following=False):
     dao = DjangoTripleDAO()
     follower_ids = []
-    for user in user_ids:
-        if following:
-            followers = twarc.following(user=user, max_results=200)
-            # followers = twarc.following(user=user, user_fields=["id", "name", "location", "username"])
-        else:
-            # followers = twarc.followers(user=user, user_fields=["id", "name", "location", "username"])
-            followers = twarc.followers(user=user, max_results=200)
-        for follower_iter in followers:
-            # time.sleep(2)
-            if "data" in follower_iter:
-                follower_datas = follower_iter["data"]
-                for follower_data in follower_datas:
-                    persist_user(follower_data)
-                    follower = follower_data["id"]
-                    follower_ids.append(follower)
-                    if follower:
-                        dao.add_follower(user, follower)
-                    else:
-                        dao.add_follower(follower, user)
+    count = 0
+    user_batches = batch(list(user_ids), 15)
+    for user_batch in user_batches:
+        for user in user_batch:
+            count += 1
+            try:
+                if following:
+                    followers = twarc.following(user=user, user_fields="id,name,location,username", max_results=500)
+                    # followers = twarc.following(user=user, user_fields=["id", "name", "location", "username"])
+                else:
+                    # followers = twarc.followers(user=user, user_fields=["id", "name", "location", "username"])
+                    followers = twarc.followers(user=user, user_fields="id,name,location,username", max_results=500)
+                for follower_iter in followers:
+                    # time.sleep(2)
+                    if "data" in follower_iter:
+                        follower_datas = follower_iter["data"]
+                        for follower_data in follower_datas:
+                            persist_user(follower_data)
+                            follower = follower_data["id"]
+                            follower_ids.append(follower)
+                            if follower:
+                                dao.add_follower(user, follower)
+                            else:
+                                dao.add_follower(follower, user)
+            except Exception as e:
+                logger.error(e)
+                logger.error("something went wrong with downloading the network")
+        # one batch finished
+        logger.debug(
+            "Going to sleep after downloading following for 15 user, {}/{} user finished".format(count, len(user_ids)))
+        sleep(15 * 60)
+
     n_level = n_level - 1
     if n_level > 0:
         download_followers(follower_ids, twarc, n_level=n_level)
--- scripts/download_networks.py
+++ scripts/download_networks.py
@@ -1,20 +1,48 @@
-from delab.models import Tweet
+import logging
 
-from delab.network.conversation_network import download_followers_recursively, download_discussion_follower
+from django.db.models import Exists, OuterRef
+
+from delab.models import Tweet, TweetAuthor, FollowerNetwork
+from delab.network.conversation_network import download_followers_recursively, get_participants
 from delab.tw_connection_util import DelabTwarc
 
 twarc = DelabTwarc()
+
+logger = logging.getLogger(__name__)
 
 
 def run():
     levels = 1
     n_conversations = 2
+    count = 0
     conversation_ids = set(Tweet.objects.values_list('conversation_id', flat=True))
-    if len(conversation_ids) > 10:
-        for conversation_id in list(conversation_ids)[:n_conversations - 1]:
-            user_ids = download_discussion_follower(conversation_id)
-            download_followers_recursively(user_ids, levels, following=False)
+    unhandled_conversation_ids = prevent_multiple_downloads(conversation_ids)
+    conversation_ids = unhandled_conversation_ids
+    if len(conversation_ids) > 10 and n_conversations > 0:
+        conversation_ids = list(conversation_ids)[:n_conversations]
+        for conversation_id in conversation_ids:
+            count += 1
+            user_ids = get_participants(conversation_id)
+            download_followers_recursively(user_ids, levels, following=True)
+            logger.debug(" {}/{} conversations finished".format(count, len(conversation_ids)))
     else:
         for conversation_id in conversation_ids:
-            user_ids = download_discussion_follower(conversation_id)
-            download_followers_recursively(user_ids, levels)
+            count += 1
+            user_ids = get_participants(conversation_id)
+            download_followers_recursively(user_ids, levels, following=True)
+            logger.debug(" {}/{} conversations finished".format(count, len(conversation_ids)))
+
+
+def prevent_multiple_downloads(conversation_ids):
+    unhandled_conversation_ids = []
+    for conversation_id in conversation_ids:
+        authors_is = set(Tweet.objects.filter(conversation_id=conversation_id).values_list("author_id", flat=True))
+        author_part_of_networks = TweetAuthor.objects.filter(twitter_id__in=authors_is). \
+            filter(Exists(FollowerNetwork.objects.filter(source_id=OuterRef('pk')))
+                   | Exists(FollowerNetwork.objects.filter(target_id=OuterRef('pk')))).distinct()
+        assert len(authors_is) > 0
+        if not len(author_part_of_networks) > len(authors_is) / 2:
+            unhandled_conversation_ids.append(conversation_id)
+        else:
+            logger.debug("conversation {} has been handled before".format(conversation_id))
+    return unhandled_conversation_ids
