f18f5da679a08171d55032d232a63409029f4f6b
Julian Dehne <julian.dehne@gmail.com>
2022-07-20 09:48:34 +0200

    - fixed missing matplotlib

--- delab/sql/debugging.sql
+++ delab/sql/debugging.sql
@@ -2,9 +2,8 @@ select count( distinct dt.conversation_id) from delab_tweet dt inner join delab_
 select count(*) from delab_moderationcandidate2;
 select count(*) from delab_tweet;
 
-delete from delab_moderationcandidate2 where tweet_id in
-                                             (select dt.id from delab_tweet dt where dt.platform = 'reddit' and dt.tn_parent_id is null)
-                                         and id not in
+select * from delab_moderationcandidate2 where tweet_id in
+                                             (select dt.id from delab_tweet dt where dt.platform = 'twitter' and dt.simple_request_id == )
+                                         and id in
                                              (select mod_candidate_id from delab_moderationrating);
 
-delete from delab_tweet where platform = 'reddit';
