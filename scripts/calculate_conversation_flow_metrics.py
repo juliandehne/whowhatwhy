2466f40a0075bdd898aa4ac5e0518834a1176da5
Julian Dehne <julian.dehne@gmail.com>
2022-09-28 10:32:08 +0200

    - added table for conversation_flow metrics - changed comment of pipeline_script in order to reference it to the moderation 1 approach

--- scripts/calculate_conversation_flow_metrics.py
+++ scripts/calculate_conversation_flow_metrics.py
@@ -1,2 +1,7 @@
 def run():
+    """
+    for each parent and child in a conversation this computes deltas of the tweet specific measures like sentiment
+    and stores it in the ConversationFlowMetric table
+    :return:
+    """
     pass
