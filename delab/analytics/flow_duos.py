f6c83069524dfdd2919d7e267fc74b95e3323344
Julian Dehne <julian.dehne@gmail.com>
2022-11-14 11:38:16 +0100

    - fixed bug in duo flows (with mentions structure or some undefined trees)

--- delab/analytics/flow_duos.py
+++ delab/analytics/flow_duos.py
@@ -78,16 +78,13 @@ class FlowDuoWindow(FLowDuo):
         intersection_ids: list = set(flow_1_ids).intersection(set(flow_2_ids))
         branching_index = max([flow_2_ids.index(intersect_id) for intersect_id in intersection_ids])
         intersection_id = flow_1_ids[branching_index]
+        branching_index, intersection_id = self.compute_intersection_id_in_weird_trees(branching_index, flow_1_ids,
+                                                                                       flow_2_ids, intersection_id)
         for tweet in self.tweets1:
             if tweet.twitter_id != intersection_id:
                 self.common_tweets.append(tweet)
             else:
                 break
-        if intersection_id not in flow_2_ids:
-            raise Exception("something weird happened in the flow_duo window computation")
-        if (flow_2_ids.index(
-            intersection_id) != branching_index):
-            raise Exception("something wrong with flow duos")
         assert flow_2_ids.index(
             intersection_id) == branching_index, "the branching index should be the same for both branches"
         start_index_pre_branching = max(branching_index - pre_branch_length, 0)
@@ -95,6 +92,19 @@ class FlowDuoWindow(FLowDuo):
         end_index_post_branching = min(branching_index + 1 + post_branch_length, len(self.tweets1), len(self.tweets2))
         self.tweets1_post_branching = self.tweets1[branching_index + 1: end_index_post_branching]
         self.tweets2_post_branching = self.tweets2[branching_index + 1: end_index_post_branching]
+
+    def compute_intersection_id_in_weird_trees(self, branching_index, flow_1_ids, flow_2_ids, intersection_id):
+        # this computes the duo_flow common ids in case of a mention (non-deterministic tree structure)
+        if intersection_id not in flow_2_ids or (flow_2_ids.index(intersection_id) != branching_index):
+            branching_index = 0
+            for id in flow_1_ids:
+                for id2 in flow_2_ids:
+                    if id == id2:
+                        branching_index += 1
+                    else:
+                        break
+        intersection_id = flow_1_ids[branching_index]
+        return branching_index, intersection_id
 
 
 def get_flow_duos(n, metric=CURRENT_DUO_FLOW_METRIC):
@@ -108,15 +118,16 @@ def get_flow_duos(n, metric=CURRENT_DUO_FLOW_METRIC):
     # constraints
 
     flow_duos = compute_flow_duos(min_length_flows, min_post_branching, min_pre_branching, metric)
-    flow_duo_pairs = sorted(flow_duos.keys(), key=lambda x: flow_duos[x])
+    flow_duo_pairs = sorted(flow_duos.keys(), key=lambda x: flow_duos[x], reverse=True)
     result = []
     for name1, name2 in flow_duo_pairs[:n]:
         tweets1 = ConversationFlow.objects.filter(flow_name=name1).get().tweets.all()
         tweets2 = ConversationFlow.objects.filter(flow_name=name2).get().tweets.all()
+        toxic_delta = flow_duos[(name1, name2)]
         flow_duo_result = FLowDuo(
             name1=name1,
             name2=name2,
-            toxic_delta=flow_duos[(name1, name2)],
+            toxic_delta=toxic_delta,
             tweets1=tweets1,
             tweets2=tweets2
         )
@@ -166,58 +177,70 @@ def compute_flow_duos(min_length_flows, min_post_branching, min_pre_branching, m
             else:
                 candidate_flows.append((name, tweets))
 
-        for name, tweets in candidate_flows:
-            for name_2, tweets_2 in candidate_flows:
-                if name_2 != name:
-                    tweet_ids = set([tweet.twitter_id for tweet in tweets])
-                    tweet2_ids = set([tweet.twitter_id for tweet in tweets_2])
-                    n_pre_branching = len(tweet_ids.intersection(tweet2_ids))
-                    n_smaller_flow = min(len(tweet_ids), len(tweet2_ids))
-                    if n_pre_branching < min_pre_branching or (n_smaller_flow - n_pre_branching) < min_post_branching:
-                        continue
-                    else:
-                        pos_toxicity = 0
-                        for positive_tweet in tweets:
-                            if metric == DUOFLOW_METRIC.TOXICITY:
-                                if positive_tweet.is_toxic and verbose:
-                                    print("adding a toxic value on plus")
-                                if positive_tweet.toxic_value is not None:
-                                    pos_toxicity += positive_tweet.toxic_value
-                            else:
-                                if positive_tweet.sentiment_value is not None:
-                                    pos_toxicity += positive_tweet.sentiment_value
-                        pos_toxicity = pos_toxicity / len(tweets)
-
-                        neg_toxicity = 0
-                        for neg_tweet in tweets_2:
-                            if metric == DUOFLOW_METRIC.TOXICITY:
-                                if neg_tweet.is_toxic and verbose:
-                                    print("adding a toxic value on minus")
-                                if neg_tweet.toxic_value is not None:
-                                    neg_toxicity += neg_tweet.toxic_value
-                            else:
-                                if neg_tweet.sentiment_value is not None:
-                                    neg_toxicity += neg_tweet.sentiment_value
-                        neg_toxicity = neg_toxicity / len(tweets_2)
-
-                        if metric == DUOFLOW_METRIC.TOXICITY:
-                            tox_delta = abs(pos_toxicity - neg_toxicity)
-                        else:
-                            tox_delta = 0
-                            if (pos_toxicity <= 0 and neg_toxicity <= 0) or (pos_toxicity >= 0 and neg_toxicity >= 0):
-                                tox_delta = abs(abs(pos_toxicity) - abs(neg_toxicity))
-                            else:
-                                if pos_toxicity >= 0 >= neg_toxicity:
-                                    tox_delta = pos_toxicity + abs(neg_toxicity)
-                                if neg_toxicity >= 0 >= pos_toxicity:
-                                    tox_delta = neg_toxicity + abs(pos_toxicity)
-                        max_delta = max(max_delta, tox_delta)
-                        flow_duos[(name, name_2)] = tox_delta
-                        # print("current_highest_delta is {}, after processing acceptable {} flowduos".format(max_delta,
-                        #                                                                                    len(flow_duos)))
+        conversation_flow_duo_candidate, conversation_max = compute_highest_flow_delta(candidate_flows,
+                                                                                       metric,
+                                                                                       min_post_branching,
+                                                                                       min_pre_branching, verbose)
+        flow_duos[conversation_flow_duo_candidate] = conversation_max
+    max_delta = max(flow_duos.values())
     logger.debug(
         "current_highest_delta is {}, after processing acceptable {} flowduos".format(max_delta, len(flow_duos)))
     return flow_duos
+
+
+def compute_highest_flow_delta(candidate_flows, metric, min_post_branching, min_pre_branching, verbose):
+    conversation_delta_highscore = 0  # assert that there is only one flowduo candidate per conversation
+    conversation_flow_duo_candidate = None
+    for name, tweets in candidate_flows:
+        for name_2, tweets_2 in candidate_flows:
+            if name_2 != name:
+                tweet_ids = set([tweet.twitter_id for tweet in tweets])
+                tweet2_ids = set([tweet.twitter_id for tweet in tweets_2])
+                n_pre_branching = len(tweet_ids.intersection(tweet2_ids))
+                n_smaller_flow = min(len(tweet_ids), len(tweet2_ids))
+                if n_pre_branching < min_pre_branching or (n_smaller_flow - n_pre_branching) < min_post_branching:
+                    continue
+                else:
+                    pos_toxicity = 0
+                    for positive_tweet in tweets:
+                        if metric == DUOFLOW_METRIC.TOXICITY:
+                            if positive_tweet.is_toxic and verbose:
+                                print("adding a toxic value on plus")
+                            if positive_tweet.toxic_value is not None:
+                                pos_toxicity += positive_tweet.toxic_value
+                        else:
+                            if positive_tweet.sentiment_value is not None:
+                                pos_toxicity += positive_tweet.sentiment_value
+                    pos_toxicity = pos_toxicity / len(tweets)
+
+                    neg_toxicity = 0
+                    for neg_tweet in tweets_2:
+                        if metric == DUOFLOW_METRIC.TOXICITY:
+                            if neg_tweet.is_toxic and verbose:
+                                print("adding a toxic value on minus")
+                            if neg_tweet.toxic_value is not None:
+                                neg_toxicity += neg_tweet.toxic_value
+                        else:
+                            if neg_tweet.sentiment_value is not None:
+                                neg_toxicity += neg_tweet.sentiment_value
+                    neg_toxicity = neg_toxicity / len(tweets_2)
+
+                    if metric == DUOFLOW_METRIC.TOXICITY:
+                        tox_delta = abs(pos_toxicity - neg_toxicity)
+                    else:
+                        tox_delta = 0
+                        if (pos_toxicity <= 0 and neg_toxicity <= 0) or (pos_toxicity >= 0 and neg_toxicity >= 0):
+                            tox_delta = abs(abs(pos_toxicity) - abs(neg_toxicity))
+                        else:
+                            if pos_toxicity >= 0 >= neg_toxicity:
+                                tox_delta = pos_toxicity + abs(neg_toxicity)
+                            if neg_toxicity >= 0 >= pos_toxicity:
+                                tox_delta = neg_toxicity + abs(pos_toxicity)
+                    # max_delta = max(max_delta, tox_delta)
+                    if tox_delta > conversation_delta_highscore:
+                        conversation_delta_highscore = tox_delta
+                        conversation_flow_duo_candidate = (name, name_2)
+    return conversation_flow_duo_candidate, conversation_delta_highscore
 
 
 def flow_duos2flow_windows(dual_flows, post_branch_length=5, pre_branch_length=5):
