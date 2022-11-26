56af828a408cb15f4d3cb6902e9ce62724935a55
Julian Dehne <julian.dehne@gmail.com>
2022-11-26 18:47:34 +0100

    - working on creating a progress report for the moderation mining project

--- scriptsql/moderation_mining_intercoder.sql
+++ scriptsql/moderation_mining_intercoder.sql
@@ -1,5 +1,5 @@
 
-select dt.text, ds.created_at, mr.u_moderating_part, mr.u_mod_rating, mr2.u_mod_rating, mr.mod_coder_id, mr2.mod_coder_id
+select dt.text, ds.created_at, ds.title, mr.u_moderating_part, mr.u_mod_rating, mr2.u_mod_rating, mr.mod_coder_id, mr2.mod_coder_id
 from delab_moderationrating mr
     join delab_moderationcandidate2 d on mr.mod_candidate_id = d.id
     join delab_moderationrating mr2 on mr2.mod_candidate_id = d.id
@@ -17,4 +17,13 @@ from delab_moderationrating mr
     join delab_simplerequest ds on dt.simple_request_id = ds.id
 where mr.u_moderating_part is not null
   and mr.u_moderating_part <> ''
-  and mr.u_mod_rating = 0
+  and mr.u_mod_rating = 0;
+
+select ds.created_at, ds.title, mr.u_mod_rating, mr.mod_coder_id
+from delab_moderationrating mr
+    join delab_moderationcandidate2 d on mr.mod_candidate_id = d.id
+    join delab_tweet dt on dt.id = d.tweet_id
+    join delab_simplerequest ds on dt.simple_request_id = ds.id
+where mr.u_moderating_part is not null
+  and mr.u_moderating_part <> ''
+
