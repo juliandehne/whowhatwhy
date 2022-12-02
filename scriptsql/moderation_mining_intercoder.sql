4547faa0cfb840d345b834497d1b70b81cb55e69
Julian Dehne <julian.dehne@gmail.com>
2022-12-02 11:05:01 +0100

    - added relabel_column

--- scriptsql/moderation_mining_intercoder.sql
+++ scriptsql/moderation_mining_intercoder.sql
@@ -25,5 +25,12 @@ from delab_moderationrating mr
     join delab_tweet dt on dt.id = d.tweet_id
     join delab_simplerequest ds on dt.simple_request_id = ds.id
 where mr.u_moderating_part is not null
-  and mr.u_moderating_part <> ''
+  and mr.u_moderating_part <> '';
 
+select mr.id, dt.text, ds.created_at, ds.title, mr.u_moderating_part, mr.u_mod_rating, mr2.u_mod_rating, mr.mod_coder_id, mr2.mod_coder_id
+from delab_moderationrating mr
+    join delab_moderationcandidate2 d on mr.mod_candidate_id = d.id
+    join delab_moderationrating mr2 on mr2.mod_candidate_id = d.id
+    join delab_tweet dt on dt.id = d.tweet_id
+    join delab_simplerequest ds on dt.simple_request_id = ds.id
+where mr.mod_coder_id = mr2.mod_coder_id and mr.id <> mr2.id;
