8653929012b71e12f6f3fb9c9d49404ebd97a59b
Julian Dehne <julian.dehne@gmail.com>
2022-12-02 09:52:41 +0100

    - added relabel_column

--- delab/models/moderation_project_2_models.py
+++ delab/models/moderation_project_2_models.py
@@ -57,6 +57,7 @@ class ModerationRating(models.Model):
     u_moderating_part = models.TextField(null=True, blank=True,
                                          help_text="Please copy the part of the tweet that is moderating to here!")
 
+    relabel_count = models.IntegerField(default=0, help_text="the number of times this rating was changed")
     # recoded = models.BooleanField
 
 
