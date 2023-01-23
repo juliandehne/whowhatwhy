8b42401cae03fbbe014e86d54a15cdebbe106af4
pvonderhaar <a.vonderhaar01@stud.uni-goettingen.de>
2023-01-23 16:21:05 +0100

    - added phrases and authors to the climate change projekt

--- scripts/download_conversations_climatechange.py
+++ scripts/download_conversations_climatechange.py
@@ -13,11 +13,38 @@ CLIMATE_AUTHOR_PROJECT = 'clima_strat_comm_author_project'
 
 
 def run():
+    words_en = ["climate change", "climate justice",
+                "climate activism", "climate activist",
+                "global warming", "greenhouse effect",
+                "air pollution", "emissions"]
+
+    words_de = ["Klimawandel", "Klimagerechtigkeit", "Klimaktivismus",
+                "Klimaaktivist", "Erderwärmung",
+                "Klimakatastrophe", "Umweltverschmutzung",
+                "Luftverschmutzung", "Zwei Grad Ziel",
+                "Treibhauseffekt", "Emissionen"]
+
+    words_de_unclear = ["Nachhaltigkeit", "Luftqualität", "Biodiversität"]
+    words_en_unclear = ["sustainability", "air qualitiy", "pollution", "biodiversity"]
     authors = ClimateAuthor.objects.all()
     for author in authors:
         lang = LANGUAGE.ENGLISH
         if author.language == 'de':
             lang = LANGUAGE.GERMAN
-        download_conversations(topic_string=CLIMATE_AUTHOR_PROJECT,
-                               query_string="(\"climate change\" OR Klimawandel OR \"climate justice\" OR Klimagerechtigkeit) from:" + author.twitter_account,
-                               language=lang)
+            for word in words_de:
+                download_conversations(topic_string=CLIMATE_AUTHOR_PROJECT,
+                                       query_string="(\"" + word + "\" ) from:" + author.twitter_account,
+                                       language=lang)
+            for word in words_de_unclear:
+                download_conversations(topic_string=CLIMATE_AUTHOR_PROJECT,
+                                       query_string="(\"" + word + "\" Klima) from:" + author.twitter_account,
+                                       language=lang)
+        else:
+            for word in words_en:
+                download_conversations(topic_string=CLIMATE_AUTHOR_PROJECT,
+                                       query_string="(\"" + word + "\" ) from:" + author.twitter_account,
+                                       language=lang)
+            for word in words_en_unclear:
+                download_conversations(topic_string=CLIMATE_AUTHOR_PROJECT,
+                                       query_string="(\"" + word + "\" climate) from:" + author.twitter_account,
+                                       language=lang)
