239e7827319c9816e1175f70f0c114de65477626
pvonderhaar <a.vonderhaar01@stud.uni-goettingen.de>
2023-07-03 14:07:24 +0200

    - started to debug current implemtation of mastodon api

--- delab/corpus/mastodon/download_conversations_mastodon.py
+++ delab/corpus/mastodon/download_conversations_mastodon.py
@@ -1,6 +1,7 @@
 from mastodon import Mastodon
 # from delab_trees import TreeManager
 from delab.corpus.twitter.download_conversations_twitter import save_tree_to_db
+from util.sql_switch import get_query_native
 from delab_trees.delab_tree import DelabTree
 from delab.models import Tweet
 from delab.delab_enums import PLATFORM
@@ -40,7 +41,6 @@ def download_conversations_to_search(query, mastodon, topic):
         else:
             context = find_context(status, mastodon)
             contexts.append(context)
-
     for context in contexts:
         conversation_id = get_conversation_id(context)
         save_toots_as_tweets(context, conversation_id)
@@ -70,12 +70,12 @@ def get_conversation_id(context):
                 return status["id"]
 
 
-
 def save_toots_as_tweets(context, conversation_id):
     descendants = context["descendants"]
     ancestors = context["ancestors"]
     origin = context["origin"]
-    root = get_root()
+    root = get_root(context)
+    #toots_in_db = toot_ids_in_db()
     for toot in ancestors:
         tweet = Tweet(twitter_id=toot["id"],
                       text=toot["content"],
@@ -86,7 +86,11 @@ def save_toots_as_tweets(context, conversation_id):
                       conversation_id=conversation_id,
                       tn_original_parent=root["id"],
                       platform=PLATFORM.MASTODON,
-                      language=toot["language"])
+                      language=toot["language"],
+                      simple_request_id=1,
+                      topic_id=2)
+        #if toot["id"] not in toots_in_db:
+        #tweet.save()
     for toot in descendants:
         tweet = Tweet(twitter_id=toot["id"],
                       text=toot["content"],
@@ -97,7 +101,11 @@ def save_toots_as_tweets(context, conversation_id):
                       conversation_id=conversation_id,
                       tn_original_parent=root["id"],
                       platform=PLATFORM.MASTODON,
-                      language=toot["language"])
+                      language=toot["language"],
+                      simple_request_id=1,
+                      topic_id=2)
+        #if toot["id"] not in toots_in_db:
+        #tweet.save()
     tweet = Tweet(twitter_id=origin["id"],
                   text=origin["content"],
                   author_id=origin["account"]["id"],
@@ -107,29 +115,47 @@ def save_toots_as_tweets(context, conversation_id):
                   conversation_id=conversation_id,
                   tn_original_parent=root["id"],
                   platform=PLATFORM.MASTODON,
-                  language=origin["language"])
+                  language=origin["language"],
+                  simple_request_id=1,
+                  topic_id=2)
+    #if origin["id"] not in toots_in_db:
+    #tweet.save()
 
 
 def get_root(context):
-    descendants = context["descendants"]
     ancestors = context["ancestors"]
     origin = context["origin"]
-    toots = descendants + ancestors + origin
-    for toot in toots:
-        if toot["in_reply_to_id"] == "null":
+    if origin["in_reply_to_id"] is None:
+        return origin
+    for toot in ancestors:
+        if toot["in_reply_to_id"] is None:
             return toot
         else:
             raise ValueError("Conversation has no root!")
 
 
 def save_toots_as_tree(context, conversation_id, topic, query):
-    root = context["origin"]
+    origin = context["origin"]
     descendants = context["descendants"]
     ancestors = context["ancestors"]  # should be emtpy
-    full_context = descendants.append(root)
-    full_context = full_context.append(ancestors)
+
     tree_context = []
-    for status in full_context:
+    tree_status = {'tree_id': conversation_id,
+                   'post_id': origin['id'],
+                   'parent_id': origin['in_reply_to_id'],
+                   'text': origin['content'],
+                   'created_at': origin['created_at'],
+                   'author_id': origin['account']['id']}
+    tree_context.append(tree_status)
+    for status in ancestors:
+        tree_status = {'tree_id': conversation_id,
+                       'post_id': status['id'],
+                       'parent_id': status['in_reply_to_id'],
+                       'text': status['content'],
+                       'created_at': status['created_at'],
+                       'author_id': status['account']['id']}
+        tree_context.append(tree_status)
+    for status in descendants:
         tree_status = {'tree_id': conversation_id,
                        'post_id': status['id'],
                        'parent_id': status['in_reply_to_id'],
