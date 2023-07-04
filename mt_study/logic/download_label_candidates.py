149216dc3bb139e455acba242c84f5e241428ce5
Julian Dehne <julian.dehne@gmail.com>
2023-07-04 16:02:31 +0200

    - changed algorithm to include more results - needs catch of 404

--- mt_study/logic/download_label_candidates.py
+++ mt_study/logic/download_label_candidates.py
@@ -18,60 +18,54 @@ from delab_trees.flow_duos import compute_flow_name
 M_TURK_TOPIC = "mturk_candidate"
 
 
-def download_mturk_sample_conversations(n_runs, platform, min_results):
+def download_mturk_sample_conversations(n_runs, platform, min_results, persist=True):
     # Perform 100 runs of the function and measure the time taken
 
-    download_mturk_sample_helper = partial(download_mturk_samples, platform, min_results, True)
+    download_mturk_sample_helper = partial(download_mturk_samples, platform, min_results, persist)
     execution_time = timeit.timeit(download_mturk_sample_helper, number=n_runs)
     average_time = (execution_time / 100) / 60
     print("Execution time:", execution_time, "seconds")
     print("Aberage Execution time:", average_time, "minutes")
 
 
-def download_mturk_samples(platform=PLATFORM.TWITTER, min_results=20, persist=True) -> list[list[DelabPost]]:
+def download_mturk_samples(platform, min_results, persist) -> list[list[DelabPost]]:
     result = []
     print("downloading random conversations for mturk_labeling")
     while len(result) < min_results:
         # try:
-        downloaded_trees = download_daily_sample(topic_string=M_TURK_TOPIC, platform=platform, persist=True)
+        downloaded_trees = download_daily_sample(topic_string=M_TURK_TOPIC, platform=platform, persist=persist)
         for tree in downloaded_trees:
             tree.validate(verbose=False)
-        result += downloaded_trees
-        print("downloaded candidates:", len(result))
-        """
-        except Exception as ex:
-            print(ex)
-            exception_counter += 1
-            if exception_counter > 100:
-                break
-        """
+        # result += downloaded_trees
+        print("downloaded candidate trees:", len(downloaded_trees))
 
-    forest = TreeManager.from_trees(result)
-    # print(f"finished downloading trees {forest}")
+        if len(downloaded_trees) > 0:
+            forest = TreeManager.from_trees(downloaded_trees)
+            # print(f"finished downloading trees {forest}")
 
-    flow_sample: list[list[DelabPost]] = forest.get_flow_sample(5, filter_function=meta_list_filter)
-    print("found flows", len(flow_sample))
+            flow_sample: list[list[DelabPost]] = forest.get_flow_sample(5, filter_function=meta_list_filter)
+            print("found flows", len(flow_sample))
+            result += flow_sample
 
-    # flow_sample = list(filter(filter_self_answers, flow_sample))
+            # flow_sample = list(filter(filter_self_answers, flow_sample))
 
-    # collect ids of the trees from the sample
-    sample_tree_ids = []
-    for sample in flow_sample:
-        first_post = sample[0]
-        tree_id = first_post.tree_id
-        sample_tree_ids.append(tree_id)
-    # throw out the trees not sampled
-    forest.keep(sample_tree_ids)
+            # collect ids of the trees from the sample
+            sample_tree_ids = []
+            for sample in flow_sample:
+                first_post = sample[0]
+                tree_id = first_post.tree_id
+                sample_tree_ids.append(tree_id)
+            # throw out the trees not sampled
+            forest.keep(sample_tree_ids)
 
-    # TODO: potentially remove even more samples here using Valentins IPA
-    if persist:
-        objects = Tweet.objects.filter(conversation_id__in=sample_tree_ids).values_list("conversation_id", flat=True)
-        n_stored_objects = len(set(list(objects)))
-        n_sample_tree_ids = len(set(sample_tree_ids))
-        assert n_stored_objects == n_sample_tree_ids
-        persist_flow_in_db(flow_sample)
-    # TODO persist flow_sample
-    return flow_sample
+            # potentially remove even more samples here using Valentins IPA
+            if persist:
+                objects = Tweet.objects.filter(conversation_id__in=sample_tree_ids).values_list("conversation_id", flat=True)
+                n_stored_objects = len(set(list(objects)))
+                n_sample_tree_ids = len(set(sample_tree_ids))
+                assert n_stored_objects == n_sample_tree_ids
+                persist_flow_in_db(flow_sample)
+    return result
 
 
 def persist_flow_in_db(flow_sample: list[list[DelabPost]]):
